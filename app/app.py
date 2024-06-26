d917737c3a2188facdf0c74176cbacdb1a49c9f3
Adrian Palacios <78890986+AdrianP17@users.noreply.github.com>
2024-06-26 01:22:46 -0500

    last commit

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from flask_mysqldb import MySQL
 import MySQLdb.cursors
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
+from babel.dates import format_date
 import openai
 import markdown2
 app = Flask(__name__)
@@ -12,7 +13,7 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '1234'
 app.config['MYSQL_DB'] = 'gym1'
-
+client = openai.Client(api_key="sk-proj-oGUxn4Z5Tr8G7NbQSeJeT3BlbkFJ4hgS0UUOq1tkKuofv1EB")
 conexion = MySQL(app)
 
 def getUserData(usuario):
@@ -32,6 +33,83 @@ def getUserData(usuario):
                 'edad': user_info[6]
             }
             return user_data
+
+
+
+@app.route('/api/estadisticas')
+def estadisticas():
+    usuario = session.get('usuario')
+    cursor = conexion.connection.cursor(MySQLdb.cursors.DictCursor)
+    query = """
+    SELECT YEARWEEK(fecha, 1) AS semana,
+           COUNT(*) AS numero_de_sesiones,
+           SUM(numSets) AS sets,
+           SUM(volumen) AS total_volumen
+    FROM Sesion
+    WHERE usuario = %s
+      AND YEAR(fecha) = YEAR(CURDATE())
+      AND MONTH(fecha) = MONTH(CURDATE())
+    GROUP BY YEARWEEK(fecha, 1)
+    ORDER BY semana;
+    """
+    cursor.execute(query,(usuario,))
+    rows = cursor.fetchall()
+    
+    total_sesiones = sum(row["numero_de_sesiones"] for row in rows)
+    total_sets = sum(row["sets"] for row in rows)
+
+    data = {
+        "semanas": [f"Semana {i+1}" for i in range(len(rows))],
+        "volumen": [row["total_volumen"] for row in rows],
+        "total_sesiones": total_sesiones,
+        "total_sets": total_sets
+    }
+
+    return jsonify(data)
+
+@app.route('/api/checklist_semanal')
+def checklist_semanal():
+    usuario = session.get('usuario')
+    cursor = conexion.connection.cursor(MySQLdb.cursors.DictCursor)
+    query = """
+    SELECT DATE(fecha) AS dia
+    FROM Sesion
+    WHERE usuario = %s
+      AND YEARWEEK(fecha, 1) = YEARWEEK(CURDATE(), 1);
+    """
+    cursor.execute(query, (usuario,))
+    rows = cursor.fetchall()
+    
+    dias_con_sesion = [row['dia'].strftime('%Y-%m-%d') for row in rows]
+    
+    # Obtener los 7 días de la semana actual
+    hoy = datetime.now()
+    inicio_semana = hoy - timedelta(days=hoy.weekday())  # Lunes de la semana actual
+    dias_semana = [(inicio_semana + timedelta(days=i)).strftime('%Y-%m-%d') for i in range(7)]
+
+    data = {
+        "dias_semana": dias_semana,
+        "dias_con_sesion": dias_con_sesion
+    }
+
+    return jsonify(data)
+
+@app.route('/api/sets_por_sesion', methods=['GET'])
+def obtener_sets_por_sesion():
+    usuario = session.get('usuario')
+    cursor = conexion.connection.cursor(MySQLdb.cursors.DictCursor)
+    
+    query = """
+    SELECT Fecha, numSets
+    FROM Sesion
+    WHERE usuario = %s
+    ORDER BY Fecha DESC
+    LIMIT 10
+    """
+    cursor.execute(query, (usuario,))
+    sets_por_sesion = cursor.fetchall()
+    return jsonify(sets_por_sesion)
+
 
 #===============REGISTER==========================
 @app.route('/register', methods=['GET', 'POST'])
@@ -82,61 +160,6 @@ def dashboard():
     user_data = getUserData(usuario)
     return render_template('dashboard.html',**user_data)
 
-@app.route('/api/estadisticas')
-def estadisticas():
-    cursor = conexion.connection.cursor(MySQLdb.cursors.DictCursor)
-    query = """
-    SELECT YEARWEEK(fecha, 1) AS semana,
-           COUNT(*) AS numero_de_sesiones,
-           SUM(numSets) AS sets,
-           SUM(volumen) AS total_volumen
-    FROM Sesion
-    WHERE usuario = "adrianpal"
-      AND YEAR(fecha) = YEAR(CURDATE())
-      AND MONTH(fecha) = MONTH(CURDATE())
-    GROUP BY YEARWEEK(fecha, 1)
-    ORDER BY semana;
-    """
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    
-    total_sesiones = sum(row["numero_de_sesiones"] for row in rows)
-    total_sets = sum(row["sets"] for row in rows)
-
-    data = {
-        "semanas": [f"Semana {i+1}" for i in range(len(rows))],
-        "volumen": [row["total_volumen"] for row in rows],
-        "total_sesiones": total_sesiones,
-        "total_sets": total_sets
-    }
-
-    return jsonify(data)
-
-@app.route('/api/checklist_semanal')
-def checklist_semanal():
-    cursor = conexion.connection.cursor(MySQLdb.cursors.DictCursor)
-    query = """
-    SELECT DATE(fecha) AS dia
-    FROM Sesion
-    WHERE usuario = 'adrianpal'
-      AND YEARWEEK(fecha, 1) = YEARWEEK(CURDATE(), 1);
-    """
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    
-    dias_con_sesion = [row['dia'].strftime('%Y-%m-%d') for row in rows]
-    
-    # Obtener los 7 días de la semana actual
-    hoy = datetime.now()
-    inicio_semana = hoy - timedelta(days=hoy.weekday())  # Lunes de la semana actual
-    dias_semana = [(inicio_semana + timedelta(days=i)).strftime('%Y-%m-%d') for i in range(7)]
-
-    data = {
-        "dias_semana": dias_semana,
-        "dias_con_sesion": dias_con_sesion
-    }
-
-    return jsonify(data)
 
 
 @app.route('/logout')
@@ -145,9 +168,6 @@ def logout():
     return redirect(url_for('login'))
 @app.route('/')
 def home():
-    if 'usuario' in session:
-        return redirect(url_for('dashboard'))
-    else:
         return redirect(url_for('login'))
 
 
@@ -326,20 +346,7 @@ def empezar_rutina(id):
         return redirect(url_for('login'))
     usuario = session['usuario']
     cursor = conexion.connection.cursor()
-    sql = "SELECT usuario, nombre, apellido, correo, altura, peso, edad FROM usuario WHERE usuario = %s"
-    cursor.execute(sql, (usuario,))
-    user_info = cursor.fetchone()
-
-    if user_info:
-        user_data = {
-                'usuario': user_info[0],
-                'nombre': user_info[1],
-                'apellido': user_info[2],
-                'correo': user_info[3],
-                'altura': user_info[4],
-                'peso': user_info[5],
-                'edad': user_info[6]
-            }
+    user_data = getUserData(usuario)
 
     if request.method == 'POST':
         duracion = request.form['duracion']
@@ -355,15 +362,18 @@ def empezar_rutina(id):
         sets = request.form.getlist('set_id')
         reps = request.form.getlist('reps')
         pesos = request.form.getlist('peso')
+        set_completed = request.form.getlist('set_completed')
+        ejercicios_ids = [request.form.get(f'ejercicio_id_{set_id}') for set_id in sets]
         volumen_total = 0
         num_sets_realizados = 0
 
         for i in range(len(sets)):
             if reps[i] and pesos[i]:  # Verificar que los valores estén presentes
-                sql_insert_set = "INSERT INTO setejercicio (Reps, Peso, Ejercicio_idEjercicio, sesion) VALUES (%s, %s, %s, %s)"
-                cursor.execute(sql_insert_set, (reps[i], pesos[i], request.form[f'ejercicio_id_{sets[i]}'], sesion_id))
-                volumen_total += int(reps[i]) * int(pesos[i])
-                num_sets_realizados += 1
+                 if set_completed[i] == '1':  # Verificar que el set esté marcado como completado
+                    sql_insert_set = "INSERT INTO setejercicio (Reps, Peso, Ejercicio_idEjercicio, sesion) VALUES (%s, %s, %s, %s)"
+                    cursor.execute(sql_insert_set, (reps[i], pesos[i], ejercicios_ids[i], sesion_id))
+                    volumen_total += int(reps[i]) * int(pesos[i])
+                    num_sets_realizados += 1
 
         # Actualizar la sesión con el volumen total y el número de sets realizados
         sql_update_sesion = "UPDATE sesion SET volumen = %s, numSets = %s WHERE idSesion = %s"
@@ -400,11 +410,12 @@ def empezar_rutina(id):
         for _ in range(ejercicio[3]):
             grouped_ejercicios[ejercicio_id]['sets'].append({
                 'idSet': ejercicio[0],
-                'reps': 0,
-                'peso': 0
+                'reps': "",
+                'peso': ""
             })
 
-    return render_template('empezar_rutina.html', **user_data,rutina=rutina, grouped_ejercicios=grouped_ejercicios)
+    return render_template('empezar_rutina.html', **user_data, rutina=rutina, grouped_ejercicios=grouped_ejercicios)
+
 
 
 
@@ -413,33 +424,27 @@ def empezar_rutina(id):
 def historial():
     if 'usuario' not in session:
         return redirect(url_for('login'))
+    
     usuario = session['usuario']
     cursor = conexion.connection.cursor()
-    sql = "SELECT usuario, nombre, apellido, correo, altura, peso, edad FROM usuario WHERE usuario = %s"
-    cursor.execute(sql, (usuario,))
-    user_info = cursor.fetchone()
-
-    if user_info:
-        user_data = {
-                'usuario': user_info[0],
-                'nombre': user_info[1],
-                'apellido': user_info[2],
-                'correo': user_info[3],
-                'altura': user_info[4],
-                'peso': user_info[5],
-                'edad': user_info[6]
-            }
+    user_data = getUserData(usuario)
     data = {}
     try:
-        cursor = conexion.connection.cursor()
-        sql = "SELECT idSesion, Fecha, Duracion, volumen, numSets FROM sesion WHERE usuario = %s ORDER BY Fecha"
-        cursor.execute(sql, (session['usuario'],))
+        sql = "SELECT idSesion, Fecha, Duracion, volumen, numSets FROM sesion WHERE usuario = %s ORDER BY Fecha DESC"
+        cursor.execute(sql, (usuario,))
         sesiones = cursor.fetchall()
-        data['sesiones'] = sesiones
+
+        # Formatear fechas
+        sesiones_formateadas = []
+        for sesion in sesiones:
+            fecha_formateada = format_date(sesion[1], format='long', locale='es')
+            sesiones_formateadas.append((sesion[0], fecha_formateada, sesion[2], sesion[3], sesion[4]))
+
+        data['sesiones'] = sesiones_formateadas
         data['mensaje'] = 'Exito' if sesiones else 'No tienes historial de sesiones.'
     except Exception as ex:
         data['mensaje'] = 'Error...'
-    return render_template('historial.html', **user_data,data=data)
+    return render_template('historial.html', **user_data, data=data)
 
 @app.route('/historial/<int:id_sesion>')
 def detalle_sesion(id_sesion):
@@ -555,6 +560,16 @@ def asistente():
     # Convertir el historial en el formato necesario para la API de OpenAI
     session['chat_history'] = [{"role": row[0], "content": row[1]} for row in chat_history]
 
+    # Añadir mensaje de bienvenida si el historial está vacío
+    if not chat_history:
+        welcome_message = "Hola! Soy tu asistente virtual para rutinas de ejercicio. ¿En qué puedo ayudarte hoy?"
+        session['chat_history'].append({"role": "assistant", "content": welcome_message})
+
+        # Guardar el mensaje de bienvenida en la base de datos
+        sql_insert = "INSERT INTO chat_history (usuario, role, content) VALUES (%s, %s, %s)"
+        cursor.execute(sql_insert, (usuario, "assistant", welcome_message))
+        conexion.connection.commit()
+
     data = {}
     if request.method == 'POST':
         user_input = request.form['user_input']
@@ -564,11 +579,11 @@ def asistente():
         response = client.chat.completions.create(
             model="gpt-3.5-turbo-0125",
             messages=[
-                {"role": "system", "content": "Es un asistente serio.","max_tokens": 200},
+                {"role": "system", "content": "Es un asistente serio que solo ayuda con elaboración de rutinas de ejercicio.","max_tokens": 200},
 
         ] + session['chat_history'],
         temperature=0.2,
-        max_tokens=200,
+        max_tokens=250,
         )
         bot_response = response.choices[0].message.content
         session['chat_history'].append({"role": "assistant", "content": bot_response})
